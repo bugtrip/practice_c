0d40e721a939e4dbf5a8a2ad635e53fe57375e3e
dev <dev.bg@yandex.com>
2025-07-13 17:46:46 +0300

    upd Readme.md

--- src/Game_board/game_board.c
+++ src/Game_board/game_board.c
@@ -5,7 +5,7 @@
 #define SIZE    10
 #define MINES   12
 
-void set_mines(char** field, char ch){
+void set_mines(char** board, char ch){
     int rows = 0;
     int cols = 0;
     int cnt = 0;
@@ -15,46 +15,46 @@ void set_mines(char** field, char ch){
     while(cnt < MINES){
         rows = rand() % (max - min + 1) + min;
         cols = rand() % (max - min + 1) + min;
-        if(field[rows][cols] != ch && 
-        (field[rows][cols+1] != ch && field[rows][cols-1] != ch) && 
-        (field[rows+1][cols] != ch && field[rows-1][cols] != ch) && 
-        (field[rows+1][cols-1] != ch && field[rows+1][cols+1] != ch) && 
-        (field[rows-1][cols-1] != ch && field[rows-1][cols+1] != ch)){
-            field[rows][cols] = ch;
+        if(board[rows][cols] != ch && 
+        (board[rows][cols+1] != ch && board[rows][cols-1] != ch) && 
+        (board[rows+1][cols] != ch && board[rows-1][cols] != ch) && 
+        (board[rows+1][cols-1] != ch && board[rows+1][cols+1] != ch) && 
+        (board[rows-1][cols-1] != ch && board[rows-1][cols+1] != ch)){
+            board[rows][cols] = ch;
             cnt++;
         }
     }
 }
 
-void print_field(char** field){
+void print_board(char** board){
     for(size_t i = 0; i < SIZE; ++i){
         for(size_t j = 0; j < SIZE; ++j){
-            printf("%c ", field[i][j]);
+            printf("%c ", board[i][j]);
             if(j == 9){ printf("\n"); }
         }
     }
 }
 
 int main(){
-    char** field = malloc(sizeof(char*) * SIZE);
+    char** board = malloc(sizeof(char*) * SIZE);
     for(size_t i = 0; i < SIZE; ++i){
-        field[i] = malloc(sizeof(char) * SIZE);
+        board[i] = malloc(sizeof(char) * SIZE);
     }
         
     for(size_t i = 0; i < SIZE; ++i){
         for(size_t j = 0; j < SIZE; ++j){
-            field[i][j] = '0'; 
+            board[i][j] = '0'; 
         }
     }
     char ch = '*';
-    set_mines(field, ch);
+    set_mines(board, ch);
 
-    print_field(field);
+    print_board(board);
 
     for(size_t i = 0; i < SIZE; ++i){
-        free(field[i]);
+        free(board[i]);
     }
-    free(field);
+    free(board);
 
     return 0;
 }
