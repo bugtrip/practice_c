17592b82b762e924ebfc151e515e424fae3672a0
dev <dev.bg@yandex.com>
2025-07-29 00:13:49 +0300

    update Makefile in Double_array_struct/

--- src/Lifo/create_node.c
+++ src/Lifo/create_node.c
@@ -1,7 +1,7 @@
 #include "create_node.h"
 
 Node* create_node(int elem, int ratio, ptr_geom_progression calc_progression){
-    Node* cur = malloc(sizeof(Node));
+    Node* cur = (Node*)malloc(sizeof(Node));
     cur->first_elem = elem < MAX_FIRST_ELEM ? elem : MAX_FIRST_ELEM;
     cur->common_ratio = ratio < MAX_COMMON_RATIO ? ratio : MAX_COMMON_RATIO;
     calc_progression(cur);
