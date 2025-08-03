d59cdd273322e6cdee66eedd7554be5488678b26
dev <dev.bg@yandex.com>
2025-08-04 00:23:45 +0300

    upd /Lifo/push.c

--- src/Lifo/create_node.c
+++ src/Lifo/create_node.c
@@ -1,11 +1,11 @@
 #include "create_node.h"
 
 Node* create_node(int elem, int ratio, ptr_geom_progression calc_progression){
-    Node* cur = (Node*)malloc(sizeof(Node));
-    if(cur == NULL){ return NULL; }
+	Node* cur = (Node*)malloc(sizeof(Node));
+	if(cur == NULL){ return NULL; }
 	cur->first_elem = elem < MAX_FIRST_ELEM ? elem : MAX_FIRST_ELEM;
-    cur->common_ratio = ratio < MAX_COMMON_RATIO ? ratio : MAX_COMMON_RATIO;
-    calc_progression(cur);
+	cur->common_ratio = ratio < MAX_COMMON_RATIO ? ratio : MAX_COMMON_RATIO;
+	calc_progression(cur);
     
     return cur;
 }
--- src/Lifo/push.c
+++ src/Lifo/push.c
@@ -1,22 +1,21 @@
 #include "push.h"
-//#include "stack.h"
 #include "geometric_progression.h"
 #include "top.h"
 
 void push(Stack* st, ptr_create_node new_node, int elem, int ratio){
     if(st->head == NULL){
-        Node* cur = new_node(elem, ratio, geometric_progression);
-        if(cur == NULL){ return ; }
+		Node* cur = new_node(elem, ratio, geometric_progression);
+		if(cur == NULL){ return ; }
 		cur->next = NULL;
-        st->head = cur;
+		st->head = cur;
         
         st->size_st++;
     } else {
-        Node* cur = top(st);
-        cur->next = new_node(elem, ratio, geometric_progression);
-        if(cur->next == NULL){ return ; }
+		Node* cur = top(st);
+		cur->next = new_node(elem, ratio, geometric_progression);
+		if(cur->next == NULL){ return ; }
 		cur = cur->next;
-        cur->next = NULL;
+		cur->next = NULL;
 
         st->size_st++;
     }
